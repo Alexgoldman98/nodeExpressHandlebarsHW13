4d107ed2b98523b020becc9ddd47e5d3f5558529
Alexandra <Alexgoldman98@gmail.com>
2021-03-17 19:43:13 -0400

    adding router.get to burgerController

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,1 +1,14 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const burger = require("../models/burger");
+
+//get request to get data from burger table
+router.get("/", (req, res) => {
+  burger.all((data) => {
+    const bData = {
+        //this is what goes to handlebars
+      burgers: data,
+    };
+    res.render("index", bData);
+  });
+});
+
