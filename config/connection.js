bb2b9d40a11ba577ac6524e52a75905db0211397
Alexandra <Alexgoldman98@gmail.com>
2021-03-25 10:53:57 -0400

    adding connection code for heroku to connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: 'localhost',
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: "",
+      database: "burgers_db",
+    });
+  }
   
-    // Your port, if not 3306
-    port: 3306,
-  
-    // Your username
-    user: 'root',
-  
-    // Be sure to update with your own MySQL password!
-    password: 'Password!Haha',
-    database: 'burgers_db',
-})
 connection.connect();
 
 module.exports = connection;
